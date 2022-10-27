20efb611a8aa9c2ba248d498ea736ac11d1ee150
tourdesegur <106815001+tourdesegur@users.noreply.github.com>
2022-10-28 00:13:21 +0100

    Added loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import LoadingIcons from "react-loading-icons";
 
 export default function Weather() {
   let [temp, setTemp] = useState();
@@ -12,5 +13,10 @@ export default function Weather() {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${key}&units=metric`;
   axios.get(url).then(getWeather);
 
-  return <h1>The temperature in Lisbon is {Math.round(temp)}</h1>;
+  return (
+    <div>
+      <h1>The temperature in Lisbon is {Math.round(temp)}</h1>
+      <LoadingIcons.Bars />
+    </div>
+  );
 }
