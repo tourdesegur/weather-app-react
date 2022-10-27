0c8da43e7d4152d0abecd868e18d6b3909511731
tourdesegur <106815001+tourdesegur@users.noreply.github.com>
2022-10-27 23:58:04 +0100

    Created React Weather app + axios

---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  let [temp, setTemp] = useState();
+  function getWeather(response) {
+    console.log(response.data.main.temp);
+    setTemp(response.data.main.temp);
+  }
+
+  let key = "bb532f26c3227c8856eb9d787a55f819";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${key}&units=metric`;
+  axios.get(url).then(getWeather);
+
+  return <h1>The temperature in Lisbon is {Math.round(temp)}</h1>;
+}
